091d834c89e0f158b9a87ddac835881ae672120b
JSChemp1on <dimitrius.z@gmail.com>
2021-02-17 12:03:59 +0300

    1.0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Test from '../lib/indacoin-form-builder.min';
 
 
 const App = class extends Component {
     render() {
         return <>
-            <Test/>
             <h1>Test</h1>
             <div>
                 123
